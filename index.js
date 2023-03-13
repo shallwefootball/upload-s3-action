cb3211c4be36ac0499a2980003bb906866063e77
DongKeon Kim / 김동건 <amos@shallwefootball.com>
2023-03-13 19:22:48 +0900

    fix slash for windows

--- index.js
+++ index.js
@@ -3,45 +3,46 @@ const S3 = require('aws-sdk/clients/s3');
 const fs = require('fs');
 const path = require('path');
 const shortid = require('shortid');
+const slash = require('slash');
 const klawSync = require('klaw-sync');
 const { lookup } = require('mime-types');
 
 const AWS_KEY_ID = core.getInput('aws_key_id', {
-  required: true
+  required: true,
 });
 const SECRET_ACCESS_KEY = core.getInput('aws_secret_access_key', {
-  required: true
+  required: true,
 });
 const BUCKET = core.getInput('aws_bucket', {
-  required: true
+  required: true,
 });
 const SOURCE_DIR = core.getInput('source_dir', {
-  required: true
+  required: true,
 });
 const DESTINATION_DIR = core.getInput('destination_dir', {
-  required: false
+  required: false,
 });
 const ENDPOINT = core.getInput('endpoint', {
-  required: false
+  required: false,
 });
 
 const s3options = {
   accessKeyId: AWS_KEY_ID,
-  secretAccessKey: SECRET_ACCESS_KEY
-}
+  secretAccessKey: SECRET_ACCESS_KEY,
+};
 
 if (ENDPOINT) {
-  s3options.endpoint = ENDPOINT
+  s3options.endpoint = ENDPOINT;
 }
 
 const s3 = new S3(s3options);
 const destinationDir = DESTINATION_DIR === '/' ? shortid() : DESTINATION_DIR;
 const paths = klawSync(SOURCE_DIR, {
-  nodir: true
+  nodir: true,
 });
 
 function upload(params) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     s3.upload(params, (err, data) => {
       if (err) core.error(err);
       core.info(`uploaded - ${data.Key}`);
@@ -52,17 +53,19 @@ function upload(params) {
 }
 
 function run() {
-  const sourceDir = path.join(process.cwd(), SOURCE_DIR);
+  const sourceDir = slash(path.join(process.cwd(), SOURCE_DIR));
   return Promise.all(
-    paths.map(p => {
+    paths.map((p) => {
       const fileStream = fs.createReadStream(p.path);
-      const bucketPath = path.join(destinationDir, path.relative(sourceDir, p.path));
+      const bucketPath = slash(
+        path.join(destinationDir, slash(path.relative(sourceDir, p.path)))
+      );
       const params = {
         Bucket: BUCKET,
         ACL: 'public-read',
         Body: fileStream,
         Key: bucketPath,
-        ContentType: lookup(p.path) || 'text/plain'
+        ContentType: lookup(p.path) || 'text/plain',
       };
       return upload(params);
     })
@@ -70,13 +73,13 @@ function run() {
 }
 
 run()
-  .then(locations => {
+  .then((locations) => {
     core.info(`object key - ${destinationDir}`);
     core.info(`object locations - ${locations}`);
     core.setOutput('object_key', destinationDir);
     core.setOutput('object_locations', locations);
   })
-  .catch(err => {
+  .catch((err) => {
     core.error(err);
     core.setFailed(err.message);
   });
